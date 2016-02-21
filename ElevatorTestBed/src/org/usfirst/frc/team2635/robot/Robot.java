0fbab816ebc24e91d6ea905d50527c6413752ad9
2635Lakemonsters <lakemonsters.software.team@gmail.com>
2016-02-21 13:46:17 -0800

    Finished elevator test bed

--- ElevatorTestBed/src/org/usfirst/frc/team2635/robot/Robot.java
+++ ElevatorTestBed/src/org/usfirst/frc/team2635/robot/Robot.java
@@ -1,11 +1,14 @@
 
 package org.usfirst.frc.team2635.robot;
 
+
 import com.lakemonsters2635.actuator.interfaces.BaseActuator;
 import com.lakemonsters2635.actuator.modules.ActuatorSimple;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.CANTalon.TalonControlMode;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -31,16 +34,94 @@ public class Robot extends IterativeRobot
 	final int ELEVATOR_RIGHT_CHANNEL = 11;
 	final int ELEVATOR_LEFT_CHANNEL = 14;
 	
+
+	public static final int REAR_RIGHT_CHANNEL = 1;
+	public static final int MID_RIGHT_CHANNEL = 2;
+	public static final int FRONT_RIGHT_CHANNEL = 3;
+	
+	public static final int REAR_LEFT_CHANNEL = 4;
+	public static final int MID_LEFT_CHANNEL = 5;
+	public static final int FRONT_LEFT_CHANNEL = 6;
+	
+	public static final int JOYSTICK_LEFT_CHANNEL = 1;
+	public static final int JOYSTICK_RIGHT_CHANNEL = 0;
+	
+	public static final int RIGHT_Y_AXIS = 1;
+	public static final int LEFT_Y_AXIS = 1;
+	
+	public static final String DRIVE_KEY_P = "Drive P";
+	public static final String DRIVE_KEY_I = "Drive I";
+	public static final String DRIVE_KEY_D = "Drive D";
+	
+	public static final double DRIVE_P_DEFAULT = 0.0;
+	public static final double DRIVE_I_DEFAULT = 0.0;
+	public static final double DRIVE_D_DEFAULT = 0.0;
+	
+	public double speedModeScaler = 1000.0;
+
+	
+	CANTalon rearRightMotor;
+	CANTalon midRightMotor;
+	CANTalon frontRightMotor;
+	
+	CANTalon rearLeftMotor;
+	CANTalon midLeftMotor;
+	CANTalon frontLeftMotor;
+	
+	DriveThreeMotor robotDrive;
+	
 	CANTalon rightElevatorMotor;
 	CANTalon leftElevatorMotor;
 	
 	BaseActuator<Double> elevateMethod;
-	
+	Joystick rightJoystick;
+	Joystick leftJoystick;
 
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
+	public void driveInit()
+	{
+		//TODO: might have to invert the output for some of these. There is a function to do that in CANTalon
+    	rearRightMotor = new CANTalon(REAR_RIGHT_CHANNEL);
+    	//rearRightMotor.changeControlMode(TalonControlMode.Follower);
+    	//rearRightMotor.set(FRONT_RIGHT_CHANNEL);
+    	
+    	midRightMotor = new CANTalon(MID_RIGHT_CHANNEL);
+    	//midRightMotor.changeControlMode(TalonControlMode.Follower);
+    	//midRightMotor.set(FRONT_RIGHT_CHANNEL);
+    	
+    	frontRightMotor = new CANTalon(FRONT_RIGHT_CHANNEL);
+    	//frontRightMotor.changeControlMode(TalonControlMode.Speed);
+    	//frontRightMotor.setPID(DRIVE_P_DEFAULT, DRIVE_I_DEFAULT, DRIVE_D_DEFAULT);
+    	
+    	rearLeftMotor = new CANTalon(REAR_LEFT_CHANNEL);
+    	//rearLeftMotor.changeControlMode(TalonControlMode.Follower);
+    	//rearLeftMotor.set(FRONT_LEFT_CHANNEL);
+    	
+    	midLeftMotor = new CANTalon(MID_LEFT_CHANNEL);
+    	//midLeftMotor.changeControlMode(TalonControlMode.Follower);
+    	//midLeftMotor.set(FRONT_LEFT_CHANNEL);
+    	
+		frontLeftMotor = new CANTalon(FRONT_LEFT_CHANNEL);
+    	//frontLeftMotor.changeControlMode(TalonControlMode.Speed);
+    	//frontLeftMotor.setPID(DRIVE_P_DEFAULT, DRIVE_I_DEFAULT, DRIVE_D_DEFAULT);
+		robotDrive = new DriveThreeMotorTankDrive(rearRightMotor, midRightMotor, frontRightMotor, rearLeftMotor, midLeftMotor, frontLeftMotor);
+      	
+    	rightJoystick = new Joystick(JOYSTICK_RIGHT_CHANNEL);
+    	leftJoystick = new Joystick(JOYSTICK_LEFT_CHANNEL);
+
+	}
+
+	public void driveTeleop()
+	{
+	    	double RY = rightJoystick.getRawAxis(1);
+	    	double LY = -leftJoystick.getRawAxis(1);
+	    	robotDrive.drive(LY, RY);
+
+	}
+
 	public void robotInit()
 	{
 		SmartDashboard.putNumber(SETPOINT_KEY, 0.0);
@@ -49,14 +130,17 @@ public class Robot extends IterativeRobot
     	SmartDashboard.putNumber(ELEVATOR_KEY_D, ELEVATOR_D_DEFAULT);
 
 		rightElevatorMotor = new CANTalon(ELEVATOR_RIGHT_CHANNEL);
-    	rightElevatorMotor.changeControlMode(TalonControlMode.Position);
-    	rightElevatorMotor.setPID(ELEVATOR_P_DEFAULT, ELEVATOR_I_DEFAULT, ELEVATOR_D_DEFAULT);
+		//Position mode
+    	//rightElevatorMotor.changeControlMode(TalonControlMode.Position);
+    	//rightElevatorMotor.enableBrakeMode(true);
+    	//rightElevatorMotor.setPID(ELEVATOR_P_DEFAULT, ELEVATOR_I_DEFAULT, ELEVATOR_D_DEFAULT);
     	
     	leftElevatorMotor = new CANTalon(ELEVATOR_LEFT_CHANNEL);
     	leftElevatorMotor.changeControlMode(TalonControlMode.Follower);
     	leftElevatorMotor.set(ELEVATOR_RIGHT_CHANNEL);
     			
 		elevateMethod = new ActuatorSimple(rightElevatorMotor);
+		driveInit();
 	}
 
 	/**
@@ -88,11 +172,30 @@ public class Robot extends IterativeRobot
 	@Override
 	public void teleopInit()
 	{
-		rightElevatorMotor.setPID(SmartDashboard.getNumber(ELEVATOR_KEY_P), SmartDashboard.getNumber(ELEVATOR_KEY_I), SmartDashboard.getNumber(ELEVATOR_KEY_D));
+		//Position mode
+		//rightElevatorMotor.setPID(SmartDashboard.getNumber(ELEVATOR_KEY_P), SmartDashboard.getNumber(ELEVATOR_KEY_I), SmartDashboard.getNumber(ELEVATOR_KEY_D));
 	}
 	public void teleopPeriodic()
 	{
-		elevateMethod.actuate(SmartDashboard.getNumber(SETPOINT_KEY));
+		
+		//For vbus mode
+		if(rightJoystick.getRawButton(2))
+		{
+			elevateMethod.actuate(-SmartDashboard.getNumber(SETPOINT_KEY));
+		}
+		else if(rightJoystick.getRawButton(3))
+		{
+			elevateMethod.actuate(SmartDashboard .getNumber(SETPOINT_KEY));
+		}
+		else
+		{
+			elevateMethod.actuate(0.0);
+		}
+		driveTeleop();
+		//For position mode
+		//elevateMethod.actuate(SmartDashboard.getNumber(SETPOINT_KEY));
+		//SmartDashboard.putNumber("Error", rightElevatorMotor.getError());
+		//SmartDashboard.putNumber("Encoder", rightElevatorMotor.getPosition());
 	}
 
 	/**
